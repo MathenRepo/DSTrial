b3162840855c7fdc119226185563197318ce4606
Mathen <mathenkumaar.pk@gmail.com>
2021-04-24 10:47:05 +0530

    just commit

--- src/java8/Immutable.java
+++ src/java8/Immutable.java
@@ -1,5 +1,21 @@
 package java8;
 
 public final class Immutable {
+	
+	public String getField1() {
+		return field1;
+	}
 
+	public String getField2() {
+		return field2;
+	}
+
+	private final String field1 = new String();
+	
+	private final String field2 = new String();
+
+	@Deprecated
+	public void meth1() {
+		
+	}
 }
